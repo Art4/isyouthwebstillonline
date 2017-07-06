ca0c4392648983e7513e7747fb0e28363ccf6f3f
Art4 <art4@wlabs.de>
2017-07-06 12:04:59 +0200

    collation und charset auf utf8_unicode_ci geändert

--- config/config.php
+++ config/config.php
@@ -22,8 +22,8 @@ return [
 				],
 				'identifier'   => '`',
 				'table_prefix' => '',
-				'charset'      => 'utf8mb4',
-				'collation'    => 'utf8mb4_unicode_ci',
+				'charset'      => 'utf8',
+				'collation'    => 'utf8_unicode_ci',
 				'enable_cache' => true,
 			],
 			'doctrine2' => [
